fd1a48af667c1ddd9541663e5c62e76d8bc7f980
Halil Olcay <holcay92@gmail.com>
2022-07-21 15:28:52 +0300

    hooks added, now i can add element to the list on the screen and update also delete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';//to import easily ctrl space tab
 
+
 function App() {
   return (
     <div className="Todo-app">
