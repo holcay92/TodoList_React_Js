29be2bca3fc4d6f8dbdf7b28536c18103755c1b8
Halil Olcay <holcay92@gmail.com>
2022-07-21 11:57:05 +0300

    todoform todolist js files are added, button addded and text field added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';//to import easily ctrl space tab
 
 function App() {
   return (
     <div className="Todo-app">
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,32 @@
 import React, {useState} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
     const[input,setInput]=useState('')
+
+    const handleChange = e =>{
+        setInput(e.target.value);
+    }
+
+    //to prevent refreshing the page on every click we write this function
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // props.onSubmit ({
+        //     id: Math.floor(Math.random()*10000),
+        //     text: input
+        // });
+
+        setInput('');
+    }
   return (
-    <form className="todo-form">
-        <input type="text" placeholder="Add a todo" value={input}
-        name="text" className='todo-input'/>
+    <form className="todo-form" onSubmit={handleSubmit}/* to prevent refreshing the page we write this function call*/>
+        <input type="text"
+            placeholder="Add a todo"
+            value={input}
+            name="text" 
+            className='todo-input'
+            onChange={handleChange}
+            />
         <button className='todo-button'>Add todo</button>
     </form>
   )
